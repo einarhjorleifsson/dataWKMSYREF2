1f209779d4e21a90ee99717df62510e1a023fe9b
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2014-01-13 18:03:02 +0000

    seeding

---
+++ R/data.R
@@ -0,0 +1,39 @@
+#' @name csCod
+#' @title Celtic Sea cod stock
+#' @description Object containing data on Celtic Sea cod stock
+#' @docType data
+#' @usage plot(csCod)
+#' @format an \code{FLStock} object
+#' @source WKMSYREF2
+#' @author NA
+NULL
+
+#' @name isCod
+#' @title Irish Sea cod stock
+#' @description Object containing data on Irish Sea cod stock
+#' @docType data
+#' @usage plot(isCod)
+#' @format an \code{FLStock} object
+#' @source WKMSYREF2
+#' @author NA
+NULL
+
+#' @name nsCod
+#' @title North Sea cod stock
+#' @description Object containing data on North Sea cod stock
+#' @docType data
+#' @usage plot(nsCod)
+#' @format an \code{FLStock} object
+#' @source WKMSYREF2
+#' @author NA
+NULL
+
+#' @name wsCod
+#' @title West Coast of Scotland cod stock
+#' @description Object containing data on West Coast of Scotland cod stock
+#' @docType data
+#' @usage plot(wsCod)
+#' @format an \code{FLStock} object
+#' @source WKMSYREF2
+#' @author NA
+NULL
---
+++ R/dataWKMSYREF2.R
@@ -0,0 +1,10 @@
+#' @name dataWKMSYREF2-package
+#' @aliases dataWKMSYREF2
+#' @aliases dataWKMSYREF2Library
+#' @docType package
+#' @title   Title dataWKMSYREF2 package
+#' @description Contains FLStock data that were used at the WKMSYREF2 meeting
+#' @details Version 1.0: 13. January 2013
+#' @author Various
+#' @seealso link to other documents
+NULL
